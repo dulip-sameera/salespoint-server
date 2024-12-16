2c0f03eee48bfa81ba002da6387e9bcaff8b0196
Dulip Sameera <dsgitrepo101@gmail.com>
2024-12-16 05:51:11 +0530

    change order response

---
+++ src/main/java/com/salespoint/api/utils/response/OrderItemResponse.java
@@ -0,0 +1,16 @@
+package com.salespoint.api.utils.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemResponse {
+    private Long id;
+    private Integer qty;
+    private Long itemId;
+}
